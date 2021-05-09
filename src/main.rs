74fceaa6da20ffc12b555281559b13c91ade4b19
Ja-sonYun <killa30867@gmail.com>
2021-05-09 18:34:18 +0900

    loss

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,9 @@ pub mod utils {
             pub fn rand() -> f64 {
                 rand::thread_rng().gen::<f64>()
             }
+            pub fn randin(from: f64, to: f64) -> f64 {
+                rand::thread_rng().gen_range(from..to)
+            }
         }
     }
     pub mod initializer {
@@ -105,15 +108,19 @@ pub mod utils {
         // ReLU
         pub fn he_initializer(fan_in: Dimension, fan_out: Dimension) -> Weight
         {
-            // rand()
-                1.
+            rand()
         }
     }
     pub mod activations {
         pub fn sigmoid(x: f64) -> f64
         {
-            // 1. / (1. + (-x).exp())
-            x
+            1. / (1. + (-x).exp())
+        }
+        pub fn d_sigmoid_v(vec: &Vec<f64>) -> f64 {
+            vec[0] * ( 1. - vec[0] )
+        }
+        pub fn d_sigmoid(x: f64) -> f64 {
+            x * (1. - x)
         }
     }
 
@@ -123,8 +130,11 @@ pub mod utils {
         pub fn MSE(target: f64, output: f64) -> Error {
             (target - output).powi(2) / 2.
         }
-        pub fn d_MSE(vec: &Vec<f64>) -> Error {
-            -((vec[0]).powi(2) / 2.)
+        pub fn d_MSE_v(vec: &Vec<f64>) -> Error {
+            (vec[0]).powi(2) / 2.
+        }
+        pub fn d_MSE(target: f64, output: f64) -> Error {
+            -(target - output)
         }
     }
     pub mod types {
@@ -145,10 +155,11 @@ use utils::maths::*;
 struct Neuron {
     input: NumOrZeroF64,
     z: Weight,              // summed of weights
-    h: Weight,              // value that wrapped by activation func
+    o: Weight,              // value that wrapped by activation func, output
     weights: Vec<Weight>,   // weights where routing from origins
     bias: Bias,             // bias, TODO: toggle this
     loss: Error,            // local loss
+    local_loss: Vec<Weight>,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -170,16 +181,17 @@ impl Neuron {
         Neuron {
             input: x,
             z: 0.,
-            h: x.reveal(),
+            o: x.reveal(),
             weights: (0..fan_out).map(|_| he_initializer(fan_in, fan_out)).collect(),
             bias: bias,
-            loss: 0.
+            loss: 0.,
+            local_loss: (0..fan_out).map(|_| 0.).collect(),
         }
     }
 
     // W * x
     fn get_wx(&self, index: usize) -> Weight {
-        self.weights[index] * self.h
+        self.weights[index] * self.o
     }
 
     fn get_loss(&mut self) -> Error {
@@ -189,7 +201,7 @@ impl Neuron {
 
     fn update_z(&mut self, weight_sum: Weight) {
         self.z = self.bias + weight_sum;
-        self.h = sigmoid(self.z);
+        self.o = sigmoid(self.z);
     }
 }
 
@@ -199,12 +211,14 @@ struct Model
     name: &'static str,
     layers: Vec<Layer>,
     layer_size: usize,
+    lr: f64,                // learning rate
     cost: Error,
 }
 
 impl Model {
+    // TODO: optional learning rate
     fn new(name: &'static str) -> Model {
-        Model { name: name, layers: Vec::new(), layer_size: 0, cost: 0. }
+        Model { name: name, layers: Vec::new(), layer_size: 0, lr: 0.1, cost: 0. }
     }
     fn push_layer(&mut self, layer: Layer) {
         self.layers.push(layer);
@@ -213,36 +227,87 @@ impl Model {
     fn add_layer(&mut self, fan_in: Dimension, fan_out: Dimension, layer: LAYER, name: Option<&'static str>) {
         self.push_layer(Layer::new(fan_in, fan_out, layer, name));
     }
+    fn debug(&self) {
+        println!("=================================");
+        println!("| model {} -> {} layers |", self.name, self.layer_size);
+        println!("=================================");
+        for layer in &self.layers {
+            println!(",-----{}-----, fan_in:{}, fan_out:{}, type:{:?}", layer.name, layer.fan_in, layer.fan_out, layer._type);
+            println!("| -({} Neurons) => {} out", layer.neurons.len(), layer.fan_out);
+            for neuron in &layer.neurons {
+                println!("|  |-{:?}", neuron);
+            }
+            println!("`---------------------------");
+        }
+        println!("=================================");
+        println!(" * Total cost: {}", self.cost);
+    }
     fn forward_propagation(&mut self) {
         // loop layers
-        for i in 0..(self.layer_size-1) {
-            // loop each neurons
-            for k in 0..self.layers[i+1].neurons.len() as usize {
+        for i in 0..(self.layer_size) {
+            // loop for get total loss and update local losses if its output layer
+            if let LAYER::OUT(_) = self.layers[i]._type {
+                self.cost = 0.;
+                for neuron in &mut self.layers[i].neurons {
+                    self.cost = self.cost + neuron.get_loss();
+                }
+                println!("cost: {}", self.cost);
+            } else {
                 if self.layers[i].fan_out != self.layers[i+1].fan_in {
                     panic!("layer dimension mismatch!");
                 }
-                let mut weight_sum: Weight = 0.;
-                // loop for get sum of neuron's weights
-                for j in 0..self.layers[i].neurons.len() as usize {
-                    weight_sum = self.layers[i].neurons[j].get_wx(k) + weight_sum;
+                // loop each neurons
+                for k in 0..self.layers[i+1].neurons.len() as usize {
+                    let mut weight_sum: Weight = 0.;
+                    // loop for get sum of neuron's weights
+                    for j in 0..self.layers[i].neurons.len() as usize {
+                        weight_sum = self.layers[i].neurons[j].get_wx(k) + weight_sum;
+                    }
+                    self.layers[i+1].neurons[k].update_z(weight_sum);
                 }
-                self.layers[i+1].neurons[k].update_z(weight_sum);
-            }
-        }
-        // loop for get total loss and update local losses if its output layer
-        if let LAYER::OUT(_) = self.layers[self.layer_size - 1]._type {
-            for neuron in &mut self.layers[self.layer_size - 1].neurons {
-                self.cost = self.cost + neuron.get_loss();
             }
         }
     }
     fn back_propagation(&mut self) {
-        println!("{:?}", self.layers[self.layer_size-1].neurons);
-        for neuron in &self.layers[self.layer_size-1].neurons {
-            // calculate E_total
-            let mut target_output: Vec<f64> = vec![neuron.input.reveal() - neuron.z];
-            let E_total = numerical_derivative(d_MSE, &mut target_output);
-            println!("E_total {:?}", E_total);
+        for l in (0..(self.layer_size-1)).rev() {
+            if let LAYER::OUT(_) = self.layers[l+1]._type {
+                for n in 0..self.layers[l+1].neurons.len() { // 4 loop
+                    // calculate (dE_total / dh)
+                    // let mut dv_E_total: Vec<f64> = vec![self.layers[l+1].neurons[n].input.reveal() - self.layers[l+1].neurons[n].o];
+                    // let E_total = numerical_derivative(d_MSE, &mut dv_E_total)[0];
+                    let E_total = d_MSE(self.layers[l+1].neurons[n].input.reveal(), self.layers[l+1].neurons[n].o);
+
+                    // calculate (do / dz)
+                    // let mut dv_o: Vec<f64> = vec![self.layers[l+1].neurons[n].o];
+                    // let o = numerical_derivative(d_sigmoid, &mut dv_o)[0];
+                    let o = d_sigmoid(self.layers[l+1].neurons[n].o);
+                    let E_total__o = E_total * o;
+
+                    for w in 0..self.layers[l].neurons.len() { // 3 loop
+                        // calculate (dz / dW)
+                        // let mut dv_z: Vec<f64> = vec![];
+                        // (dE / dh)
+                        self.layers[l].neurons[w].local_loss[n] = E_total__o * self.layers[l].neurons[w].o;
+
+                        // update weight
+                        self.layers[l].neurons[w].weights[n] = self.layers[l].neurons[w].weights[n] - (self.lr * self.layers[l].neurons[w].local_loss[n]);
+                    }
+                }
+            } else {
+                for n in 0..self.layers[l].neurons.len() {
+                    for w in 0..self.layers[l+1].neurons.len() {
+                        // self.layers[l].neruons[n].weights[w];
+                        let dE_total_W:f64 = self.layers[l+1].neurons[w].local_loss.iter().sum();
+
+                        // let mut dh_o: Vec<f64> = vec![self.layers[l+1].neurons[w].o];
+                        // let h = numerical_derivative(d_sigmoid, &mut dh_o)[0];
+                        let h = d_sigmoid(self.layers[l+1].neurons[w].o);
+                        self.layers[l].neurons[n].local_loss[w] = dE_total_W * self.layers[l].neurons[n].o * h;
+
+                        self.layers[l].neurons[n].weights[w] = self.layers[l].neurons[n].weights[w] - (self.lr * self.layers[l].neurons[n].local_loss[w]);
+                    }
+                }
+            }
         }
         // for i in (self.layer_size)..1 {
         //     for k in 0..self.layers[i].fan_in as usize {
@@ -250,6 +315,7 @@ impl Model {
         //     }
         // }
     }
+
 }
 
 trait LayerBlock {
@@ -259,7 +325,7 @@ trait LayerBlock {
         let mut neurons: Vec<Neuron>;
         // if this layer is input or hidden,
         neurons = Vec::with_capacity(fan_in as usize);
-        for i in 0..layer.get_dim(fan_in, fan_out) as usize {
+        for i in 0..fan_in as usize {
             neurons.push(Neuron::new(layer.unwrap_with_index(i), 0., fan_in, fan_out));
         }
         // if this layer is output, which is doens't have nuerons
@@ -281,6 +347,9 @@ struct Layer
 impl LayerBlock for Layer {
     type _Layer = Layer;
     fn new(fan_in: Dimension, fan_out: Dimension, layer: LAYER, name: Option<&'static str>) -> Layer {
+        if !layer.check_size(fan_in) {
+            panic!("\n| vector and fan_in length mismatch!\n| at layer '{}', got {} size vector, expected {}\n", name.unwrap(), layer.get_len(), fan_in);
+        }
         Layer {
             name: name.unwrap(),
             neurons: Layer::init_neurons(fan_in, fan_out, &layer),
@@ -306,7 +375,9 @@ impl LAYER {
     }
     fn unwrap_with_index(&self, i: usize) -> NumOrZeroF64 {
         match &self {
-            LAYER::IN(vw) | LAYER::OUT(vw) => NumOrZeroF64::Num(vw[i]),
+            LAYER::IN(vw) | LAYER::OUT(vw) => {
+                NumOrZeroF64::Num(vw[i])
+            },
             _ => NumOrZeroF64::Zero
         }
     }
@@ -316,24 +387,39 @@ impl LAYER {
             LAYER::OUT(_) => fan_out
         }
     }
+    fn get_len(&self) -> usize {
+        match &self {
+            LAYER::IN(val) | LAYER::OUT(val) => val.len(),
+            _ => 0
+        }
+    }
+    fn check_size(&self, dim: Dimension) -> bool {
+        match &self {
+            LAYER::IN(val) | LAYER::OUT(val) => (val.len() == dim as usize),
+            _ => true
+        }
+    }
 }
 
 fn main()
 {
-    let input = vec![0.1, 0.2, 0.5];
-    let output = vec![0.4, 0.3];
+    let input = vec![0.1, 0.2];
+    let output = vec![0.4, 0.3, 0.3];
     let mut new_model = Model::new("test");
     // 0, 4
     // 4, 3
     // 3, 0
-    new_model.add_layer(3, 4, LAYER::IN(input), Some("first"));
-    new_model.add_layer(4, 4, LAYER::HIDDEN, Some("first"));
-    new_model.add_layer(4, 2, LAYER::HIDDEN, Some("first"));
-    new_model.add_layer(2, 0, LAYER::OUT(output), Some("first"));
-    // new_model.forward_propagation();
+    new_model.add_layer(2, 5, LAYER::IN(input), Some("input"));
+    new_model.add_layer(5, 3, LAYER::HIDDEN, Some("third"));
+    new_model.add_layer(3, 4, LAYER::HIDDEN, Some("third"));
+    new_model.add_layer(4, 3, LAYER::HIDDEN, Some("third"));
+    new_model.add_layer(3, 0, LAYER::OUT(output), Some("output"));
+    for i in (0..400) {
+        new_model.forward_propagation();
+        new_model.back_propagation();
+    }
+    new_model.debug();
     // new_model.back_propagation();
-    // new_model.back_propagation();
-    println!("{:?}", new_model);
     // let mut newModel = Model::<2>::new("new");
     // const A: [usize;2] = [1, 2];
 
